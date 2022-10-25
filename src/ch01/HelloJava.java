15a2418a047b38ae1090a7ffd59834fca3931cdb
SeulgiLim <71672106+SeulgiLim@users.noreply.github.com>
2022-10-25 23:47:55 +0900

    Hello Java!

--- src/ch01/HelloJava.java
+++ src/ch01/HelloJava.java
@@ -2,9 +2,8 @@ package ch01;
 
 public class HelloJava {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) { //구동하기 위해 사용하는 함수
+		System.out.println("Hello, Java");
 	}
 
 }
