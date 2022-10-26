b621b4930a605cb19ecb764fa157b3caadd50e30
SeulgiLim <71672106+SeulgiLim@users.noreply.github.com>
2022-10-26 23:46:18 +0900

    Hello Java2

--- src/ch01/HelloJava.java
+++ src/ch01/HelloJava.java
@@ -4,6 +4,9 @@ public class HelloJava {
 
 	public static void main(String[] args) { //구동하기 위해 사용하는 함수
 		System.out.println("Hello, Java");
+		System.out.println("HaHaHa");
+		System.out.println("Practice!");
+		System.out.println("zzzzzzz");
 	}
 
 }
